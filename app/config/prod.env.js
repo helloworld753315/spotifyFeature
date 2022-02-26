0695cff55c42277cb1cea0fe1e85f40929e1578d
helloworld753315 <helloworld.cpp.h@gmail.com>
2022-02-26 21:30:48 +0900

    環境変数の読み込みを本番環境にも適用

--- app/config/prod.env.js
+++ app/config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  VUE_APP_SPOTIFY_CLIENT_ID: JSON.stringify(
+    process.env.VUE_APP_SPOTIFY_CLIENT_ID
+  )
+};
